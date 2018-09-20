e150a9c4bd26f1bbd420f16fd5cd47ca05916a40
“Brian <brikoh@bu.edu>
2018-09-20 06:42:14 -0400

    updates to client

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,10 @@ class App extends Component {
 
     this.state = {
       username: '',
-      password: ''
+      password: '',
+      isLogin: true,
+      rooms: [],
+      newRoom: ''
     }
   }
 
@@ -15,11 +18,33 @@ class App extends Component {
     event.preventDefault();
 
     const {username, password} = this.state;
+    this.setState({
+      isLogin: false
+    });
+  }
+
+  createRooms() {
+    const {rooms} = this.state;
+    let rooms_render = []
+    for (let i=0; i<rooms.length; i++) {
+      rooms_render.push(<option value={rooms[i]}>{rooms[i]}</option>)
+    }
+    return rooms_render;
+  }
+
+  createRoom() {
+    const{newRoom, rooms} = this.state;
+    let newRooms = [];
+    for (let i=0; i<rooms.length; i++) {
+      newRooms.push(rooms[i])
+    }
+    newRooms.push(newRoom);
+    this.setState({rooms: newRooms});
   }
 
   render() {
-    const {username, password} = this.state;
-    const isLogin = false;
+    const {username, password, isLogin, rooms, newRoom} = this.state;
+
     if (isLogin) {
       return (
         isLogin && <form onSubmit={this.handleSubmit.bind(this)}>
@@ -47,19 +72,13 @@ class App extends Component {
             <h3 className="main-sub-title">Nabeel Younis and Brian Koh</h3>
           </div>
           <div className="room-option-container">
-            <div className="room-action">Create Room</div><select>
-              <option value="volvo">Room 1</option>
-              <option value="saab">Room 2</option>
-              <option value="mercedes">Room 3</option>
-              <option value="audi">Room 4</option>
-            </select>
+            <div className="room-action">Create Room</div>
+            <input className="room-input" name="username" onChange={e => this.setState({newRoom: e.target.value})} value={this.state.newRoom} required type="text"></input>
+            <a className="room-create" href='' onClick={this.createRoom.bind(this)}>Create</a>
             <div className="room-action">Delete Room</div><select>
-              <option value="volvo">Room 1</option>
-              <option value="saab">Room 2</option>
-              <option value="mercedes">Room 3</option>
-              <option value="audi">Room 4</option>
+              {this.createRooms()}
             </select>
-            <div className="room-action">Start Recording Temperature Sensor</div>
+            <div className="room-action">Start Temp Sensor</div>
             <a className="room-action" href=''>Start</a>
           </div>
           <div className="room-container shadow">Room 1</div>
